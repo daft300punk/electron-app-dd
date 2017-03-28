87f232f0ab1311f91ac1c6d53c17604c37a5006d
Abhilash Singh <daft300punk@gmail.com>
2017-03-28 09:04:43 +0530

    Added ModuleList component

---
+++ app/components/ModuleList/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+import styles from './index.css';
+
+export default class ModuleList extends Component {
+  render() {
+      const style = {
+      height: '250px',
+      width: '200px',
+      textAlign: 'center',
+      marginRight: '30px',
+    }
+
+    return (
+      <div className={styles.paperWrap}>
+        <Paper zDepth={1} style={style} className={styles.paperItem}>
+          <div className={styles.moduleImage}></div>
+          <div className={styles.moduleTitle}>Module 1</div>
+        </Paper>
+        <Paper zDepth={1} style={style} className={styles.paperItem}>
+          <div className={styles.moduleImage}></div>
+          <div className={styles.moduleTitle}>Module 1</div>
+        </Paper>
+        <Paper zDepth={1} style={style} className={styles.paperItem}>
+          <div className={styles.moduleImage}></div>
+          <div className={styles.moduleTitle}>Module 1</div>
+        </Paper>
+        <Paper zDepth={1} style={style} className={styles.paperItem}>
+          <div className={styles.moduleImage}></div>
+          <div className={styles.moduleTitle}>Module 1</div>
+        </Paper>
+      </div>
+    );
+  }
+}
