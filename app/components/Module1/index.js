5a730d6c7fb59c18d5ce84bf48ce9fdaa055034e
Abhilash Singh <daft300punk@gmail.com>
2017-03-29 11:00:26 +0530

    Added path movement animation

--- app/components/Module1/index.js
+++ app/components/Module1/index.js
@@ -5,6 +5,7 @@ import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import Link from 'react-router';
 import { browserHistory } from 'react-router';
+import paper, {view} from 'paper';
 
 class Module1 extends Component {
   constructor(props) {
@@ -14,6 +15,27 @@ class Module1 extends Component {
 
   componentDidMount() {
     const canvas = document.getElementById('canvas');
+    canvas.style.width = '100%';
+    paper.setup(canvas);
+
+    var path1 = new paper.Path('M98.36,214.208l2.186-1.093V210.2l-3.378,0.117l1.174,4.137L98.36,214.208z');
+    path1.strokeColor = '#f5f5f5';
+    path1.strokeWidth = 3;
+    path1.scale(50);
+    path1.visible = false;
+
+    const length = path1.length;
+
+    let pathSuperimposed = new paper.Path({
+      strokeColor: 'red',
+      strokeWidth: 4,
+      opacity: .2,
+    });
+    pathSuperimposed.onFrame = function(event) {
+      if(event.count < length / 2) {
+        pathSuperimposed.add(path1.getPointAt(event.count * 2));
+      }
+    }
   }
 
   render() {
