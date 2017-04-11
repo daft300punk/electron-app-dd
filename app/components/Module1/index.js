5f080c60d6f80bc454745fa95f658720b3924233
Abhilash Singh <daft300punk@gmail.com>
2017-04-11 13:20:24 +0530

    Modal on clicking next

--- app/components/Module1/index.js
+++ app/components/Module1/index.js
@@ -8,6 +8,7 @@ import { browserHistory } from 'react-router';
 import paper, { view } from 'paper';
 import Drawer from '../../utils/Drawer/Drawer';
 import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
 
 const SVG_ARRAY = [
   'M213.1,6.7c-32.4-14.4-73.7,0-88.1,30.6C110.6,4.9,67.5-9.5,36.9,6.7C2.8,22.9-13.4,62.4,13.5,110.9 C33.3,145.1,67.5,170.3,125,217c59.3-46.7,93.5-71.9,111.5-106.1C263.4,64.2,247.2,22.9,213.1,6.7z',
@@ -21,7 +22,22 @@ class Module1 extends Component {
     this.currentPath = 0;
     this.onClickClose = props.onClickClose;
     this.myDrawer = new Drawer();
+    this.state = {
+      open: false,
+    }
   }
+
+  handleDialogOpen = () => {
+    this.setState({
+      open: true
+    });
+  };
+
+  handleDialogClose = () => {
+    this.setState({
+      open: false
+    });
+  };
 
   componentDidMount(svgPathData) {
     this.startDrawer(SVG_ARRAY[0]);
@@ -36,8 +52,12 @@ class Module1 extends Component {
     this.myDrawer.listenForUserInput();
   }
 
-  onClickNext() {
-    this.currentPath++;
+  onClickNext = () => {
+    this.setState({
+      open: false
+    });
+
+    this.currentPath++;   
     if (SVG_ARRAY.length > this.currentPath) {
       this.myDrawer.clearEverything();
       this.startDrawer(SVG_ARRAY[this.currentPath]);
@@ -48,13 +68,24 @@ class Module1 extends Component {
   }
 
   componentWillUnmount() {
-    this.state.myDrawer.clearEverything();
+    this.myDrawer.clearEverything();
   }
 
   render() {
-    const style = {
-      background: 'rgba(0, 0, 0, .10)',
-    }
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Next"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.onClickNext}
+      />
+    ]
+
     return (
       <div className={styles.wrap}>
         <AppBar
@@ -64,10 +95,17 @@ class Module1 extends Component {
               <NavigationClose />
             </IconButton>
           }
-          iconElementRight={<FlatButton label="Finish" onClick={() => this.onClickNext()} />}
+          iconElementRight={<FlatButton label="Next" onClick={this.handleDialogOpen} />}
+        />
+        <Dialog 
+          title="See how you did"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleDialogClose}
         />
         <div className={styles.canvasWrap}>
-            <canvas id='canvas' style={style} />
+          <canvas id='canvas' />
         </div>
       </div>
     );
